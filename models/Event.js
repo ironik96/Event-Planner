fa66b7f4e49cac44621bdcf14341af28ebd67705
Ahmad AlAmiri <a.alamiri.96@gmail.com>
2022-05-29 20:32:16 +0300

    commit to delete

--- models/Event.js
+++ models/Event.js
@@ -2,10 +2,21 @@ const mongoose = require("mongoose");
 const { model, Schema } = mongoose;
 
 const eventSchema = new Schema({
-  organizer: String,
-  name: String,
-  email: String,
-  image: String,
+  organizer: { type: String, unique: true, maxLength: 20 },
+  name: { type: String, pattern: /^((?!event).)*$/ },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: "Email address is required",
+    validate: [validateEmail, "Please fill a valid email address"],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address",
+    ],
+  },
+  image: { String },
   numOfSeats: Number,
   bookedSeats: Number,
   startDate: Date,
